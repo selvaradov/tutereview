a5d835074e3cfc282436d7570560a7506469133c
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-06-29 11:59:39 +0100

    change wording

--- client/src/components/HeaderNavbar.tsx
+++ client/src/components/HeaderNavbar.tsx
@@ -18,14 +18,14 @@ const HeaderNavbar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg" className="mb-3">
       <Container>
-        <Navbar.Brand as={Link} to="/">My App</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">TuteReview</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Home</Nav.Link>
             {isAuthenticated && (
               <>
-                <Nav.Link as={Link} to="/review" active={location.pathname === '/review'}>Review</Nav.Link>
+                <Nav.Link as={Link} to="/review" active={location.pathname === '/review'}>Submit a review</Nav.Link>
                 <Nav.Link as={Link} to="/search" active={location.pathname === '/search'}>Search</Nav.Link>
               </>
             )}
