1ad30815e73a4169cc4926016c3a21283e2440a7
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-01 14:37:46 +0100

    support college info

--- server/src/models/review.ts
+++ server/src/models/review.ts
@@ -1,16 +1,18 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document, ObjectId } from 'mongoose';
 import { IUser } from './user.js';
 
 export interface IReview extends Document {
   submitter: IUser['_id'];
-  responses: any;
+  responses: { [key: string]: string };
   submittedAt: Date;
+  college: string;
 }
 
 const reviewSchema = new Schema({
   submitter: { type: Schema.Types.ObjectId, ref: 'User' },
   responses: { type: Schema.Types.Mixed },
-  submittedAt: { type: Date, default: Date.now }
+  submittedAt: { type: Date, default: Date.now },
+  college: { type: String }
 });
 
 export default mongoose.model<IReview>('Review', reviewSchema);
