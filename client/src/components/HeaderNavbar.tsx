eb40467e63659368a3a25457f3c159344cf78220
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-01 15:09:10 +0100

    add logo to navbar

--- client/src/components/HeaderNavbar.tsx
+++ client/src/components/HeaderNavbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { Navbar, Nav, Button } from 'react-bootstrap';
+import logo from '../img/logo860.png';
 
 const HeaderNavbar: React.FC = () => {
   const { isAuthenticated, logout } = useAuth();
@@ -9,24 +10,33 @@ const HeaderNavbar: React.FC = () => {
 
   return (
     <Navbar expand="lg" className="mb-3">
-        <Navbar.Brand as={Link} to="/">TuteReview</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Home</Nav.Link>
-            {isAuthenticated && (
-              <>
-                <Nav.Link as={Link} to="/review" active={location.pathname === '/review'}>Submit a review</Nav.Link>
-                <Nav.Link as={Link} to="/search" active={location.pathname === '/search'}>Search</Nav.Link>
-              </>
-            )}
-          </Nav>
+      <Navbar.Brand as={Link} to="/" className="d-flex align-items-center">
+        <img
+          src={logo}
+          width="60"
+          height="auto"
+          className="d-inline-block align-top me-2"
+          alt="TuteReview Logo"
+        />
+        TuteReview
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/" active={location.pathname === '/'}>Home</Nav.Link>
           {isAuthenticated && (
-            <Button variant="outline-danger" onClick={logout}>
-              Log Out
-            </Button>
+            <>
+              <Nav.Link as={Link} to="/review" active={location.pathname === '/review'}>Submit a review</Nav.Link>
+              <Nav.Link as={Link} to="/search" active={location.pathname === '/search'}>Search</Nav.Link>
+            </>
           )}
-        </Navbar.Collapse>
+        </Nav>
+        {isAuthenticated && (
+          <Button variant="outline-danger" onClick={logout}>
+            Log Out
+          </Button>
+        )}
+      </Navbar.Collapse>
     </Navbar>
   );
 };
