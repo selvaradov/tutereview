935b0930acebdbb86326fb09206b8a85b4cd2aa1
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-01 15:39:58 +0100

    prevent resubmission of profile data

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -16,10 +16,20 @@ router.get('/options', async (req, res) => {
 });
 
 // update profile endpoint
-router.post('/update-profile', async (req, res) => {
+router.post('/profile', async (req, res) => {
   if (!req.user) {
     console.error('Unauthorized request to update profile');
     return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    return res.status(404).json({ message: 'User not found' });
+  }
+
+  if (user.isProfileComplete) {
+    return res.status(403).json({ message: 'Profile is already complete and cannot be updated' });
   }
 
   const { college, year, subject } = req.body;
@@ -36,13 +46,35 @@ router.post('/update-profile', async (req, res) => {
       { new: true }
     );
 
+    res.json({ message: 'Profile updated successfully', user });
+  } catch (error) {
+    console.error('Error updating profile:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+// fetch profile endpoint
+router.get('/profile', async (req, res) => {
+  if (!req.user) {
+    console.error('Unauthorized request to fetch profile');
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  try {
+    const user = await User.findById(req.user.id);
+
     if (!user) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    res.json({ message: 'Profile updated successfully', user });
+    res.json({
+      college: user.college,
+      year: user.year,
+      subject: user.subject,
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Error updating profile', error });
+    console.error('Error fetching user profile:', error)
+    res.status(500).json({ error: 'Internal server error' });
   }
 });
 
