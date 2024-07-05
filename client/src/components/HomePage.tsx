1f251d26fa5c2f1bf3eabcb7f1eec7083541430c
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-05 10:25:18 +0100

    modularise welcome message

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -7,43 +7,48 @@ import FAQSection from './FAQSection';
 import PageLayout from './PageLayout';
 import { useLoading } from '../context/LoadingContext';
 
-const HomePage: React.FC = () => {
+const WelcomeMessage: React.FC = () => {
   const { isAuthenticated, isAuthInitialized, isProfileComplete, login } = useAuth();
   const { isLoading } = useLoading();
+  if (!isAuthInitialized || isLoading) {
+    return null;
+  }
+  return (
+    isAuthenticated ? (
+      isProfileComplete ? (
+        <div>
+          <p>You're all set! You can now access:</p>
+          <ul>
+            <li><Link to="/review">Review Page</Link></li>
+            <li><Link to="/search">Search Page</Link></li>
+          </ul>
+        </div>
+      ) : (
+        <div className="mb-3">
+          <p>You're logged in, but your profile is incomplete.</p>
+          <Link to="/profile">
+            <Button variant="primary">Complete your profile</Button>
+          </Link>
+        </div>
+      )
+    ) : (
+      <div className="mb-3">
+        <p>Please verify that you are a member of the university with your SSO.</p>
+        <Button onClick={login} variant="primary">Continue with Microsoft</Button>
+      </div>
+    )
+  );
+};
 
+
+const HomePage: React.FC = () => {
   useEffect(() => {
     document.title = 'TuteReview - Home'; // NOTE could be done as a hook, and with PWA check
   }, []);
 
-  if (!isAuthInitialized || isLoading) {
-    return null;
-  }
-
   return (
     <PageLayout title="Welcome to TuteReview">
-      {isAuthenticated ? (
-        isProfileComplete ? (
-          <div>
-            <p>You're all set! You can now access:</p>
-            <ul>
-              <li><Link to="/review">Review Page</Link></li>
-              <li><Link to="/search">Search Page</Link></li>
-            </ul>
-          </div>
-        ) : (
-          <div className="mb-3">
-            <p>You're logged in, but your profile is incomplete.</p>
-            <Link to="/profile">
-              <Button variant="primary">Complete your profile</Button>
-            </Link>
-          </div>
-        )
-      ) : (
-        <div className="mb-3">
-          <p>Please verify that you are a member of the university with your SSO.</p>
-          <Button onClick={login} variant="primary">Continue with Microsoft</Button>
-        </div>
-      )}
+      <WelcomeMessage />
       <AboutSection />
       <FAQSection />
     </PageLayout>
