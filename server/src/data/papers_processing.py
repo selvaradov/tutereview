588a5f854daaba447c7200d4745486bd8d87aedd
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-06 13:27:34 +0100

    improve paper validation and ID generation

--- server/src/data/papers_processing.py
+++ server/src/data/papers_processing.py
@@ -16,7 +16,7 @@ def generate_and_sort_identifiers(data):
     for subject, courses in data.items():
         for course in courses:
             try:
-                course_id = f"{subject}_{course['level']}_{course['code']}".lower()
+                course_id = f"{subject}_{course['level']}_{course['code']}".lower().replace(" ", "_")
                 course['id'] = course_id
             except KeyError:
                 print(f"Error: {course}, {subject}")
@@ -26,6 +26,22 @@ def generate_and_sort_identifiers(data):
 
 # Generate identifiers and sort the data
 sorted_data_with_ids = generate_and_sort_identifiers(data)
+
+# %%
+# Assert that all IDs are unique
+id_counts = {}
+
+for courses in sorted_data_with_ids.values():
+    for course in courses:
+        course_id = course['id']
+        if course_id in id_counts:
+            id_counts[course_id] += 1
+        else:
+            id_counts[course_id] = 1
+
+for id, count in id_counts.items():
+    if count > 1:
+        print(f"Duplicate ID found: {id}, Count: {count}")
 
 #%%
 # Save the sorted data with identifiers
