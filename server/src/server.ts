2894e57bd35bf66c1716171beb131a0c8f10752a
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-08 22:20:24 +0100

    reword log messages

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,5 +3,10 @@ import app from './app.js';
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  if (process.env.NODE_ENV === 'development'){
+    console.log(`Server is running on http://localhost:${PORT}`);
+  } else {
+    console.log(`Server is running on port ${PORT}`);
+  }
+  
 });
