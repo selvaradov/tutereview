61aad081e79571c1a04f410e514b44bccc6b0066
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-08 15:58:45 +0100

    remove cors, add helmet, tidy db setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,17 +2,18 @@
 import { loadEnvConfig } from './config/env.js' // configure environment variables
 import express, { Application, Request, Response, NextFunction } from 'express';
 import session from 'express-session';
-import connectSQLite3 from 'connect-sqlite3';
 import passport from 'passport';
 import { configurePassport } from './config/passport.js';
+import { getMongoURI } from './config/db.js';
+import { getSecret } from './config/secrets.js';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import MongoStore from 'connect-mongo';
 import logger from 'morgan';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 
 // Import routes
 import authRouter from './routes/authRoutes.js';
@@ -25,7 +26,7 @@ configurePassport();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const app: Application = express();
+const app = express();
 
 // Middleware
 app.use(express.json());
@@ -34,18 +35,14 @@ app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(cookieParser());
 
-// CORS setup
-const corsOptions = {
-  origin: process.env.CLIENT_URL,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // enable pre-flight requests for all routes
+app.use(helmet());
 
 // database setup
-const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/reviewDB';
+const mongoURI = await getMongoURI();
+if (!mongoURI) {
+  console.error('MongoURI is not set');
+  process.exit(1);
+}
 mongoose.connect(mongoURI)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
@@ -56,10 +53,15 @@ const sessionStore = MongoStore.create({
   ttl: 14 * 24 * 60 * 60, // 14 days
 })
 
+const sessionSecret = await getSecret('SESSION_SECRET')
+if (!sessionSecret) {
+  console.error('SESSION_SECRET is not set');
+  process.exit(1);
+}
 app.use(
   session({
     store: sessionStore,
-    secret: process.env.SESSION_SECRET || 'my-secret',
+    secret: sessionSecret,
     resave: false,
     saveUninitialized: true,
     cookie: {
