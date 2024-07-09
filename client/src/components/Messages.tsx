8aa1d260b4ddc3290bc11db45f6204de3871de2f
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-09 11:26:16 +0100

    separate out mailto link pieces

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,12 +1,25 @@
 import { Alert } from 'react-bootstrap';
 
 export const MissingOptionsMessage: React.FC = () => {
+  const email = "tutereview.org+support@gmail.com";
+  const subject = encodeURIComponent("Review Submission Issue");
+  const body = encodeURIComponent(`Hi,
+
+The website is missing an option.
+
+Subject:
+
+Paper:
+
+Thanks!`);
+
+  const mailtoLink = `mailto:${email}?subject=${subject}&body=${body}`;
   return (
     <Alert variant="info">
-    If the subject or paper you're looking for is not listed,
-    or the options are incorrect in some way, please contact us at {' '}
-    <a href="mailto:tutereview.org+support@gmail.com?subject=Review%20Submission%20Issue&body=Hi,%0D%0A%0D%0AThe%20website%20is%20missing%20an%20option.%0D%0A%0D%0ASubject:%20%0D%0A%0D%0APaper:%20%0D%0A%0D%0AThanks!">tutereview.org+support@gmail.com</a>
-    {' '} and we'll sort it as soon as we can.
-  </Alert>
+      If the subject or paper you're looking for is not listed,
+      or the options are incorrect in some way, please contact us at {' '}
+      <a href={mailtoLink}>tutereview.org+support@gmail.com</a>
+      {' '} and we'll sort it as soon as we can.
+    </Alert>
   );
 };
