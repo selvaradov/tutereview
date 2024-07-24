946c019edbc8acf54ee5d867d3e845498988c897
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-24 19:51:12 +0100

    setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettierConfig from 'eslint-config-prettier';
+// import prettierPlugin from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  prettierConfig,
+  // prettierPlugin
+);
