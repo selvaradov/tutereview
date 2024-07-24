bd27d012a797c8e9a8238205beea130086d69705
Rohan Selva-Radov <33783348+selvaradov@users.noreply.github.com>
2024-07-24 20:05:40 +0100

    setup prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,22 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import prettierConfig from 'eslint-config-prettier';
-// import prettierPlugin from 'eslint-plugin-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
   prettierConfig,
-  // prettierPlugin
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    ignores: ['venv/**/*'],
+  },
 );
